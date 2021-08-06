a477dc517338e2a7fc2894c0be8f47e40ba12890
Rishi pandey <50805925+Rishi500067313@users.noreply.github.com>
2021-08-07 00:03:52 +0530

    Delete Script_to_update_data.sql

--- Method - 2/Script_to_update_data.sql
+++
@@ -1,9 +0,0 @@
-use postman_data;
-
-CREATE TABLE temp_update_table (name text, sku text, description text);
-
-copy into temp_update_table from @my_csv_/products.csv.gz file_format = (format_name = mycsvformat_1) on_error = 'skip_file';
-
-update table_2 set table_2.description = temp_update_table.description from temp_update_table where temp_update_table.sku = table_2.sku;
-  
-drop table temp_update_table;
